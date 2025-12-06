357951be3ffa2eefbebba414344bc940a0a4e442
Mehmet Bektas <mbektasgh@outlook.com>
2025-12-05 20:38:02 -0800

    fix formatting

--- notebook_intelligence/util.py
+++ notebook_intelligence/util.py
@@ -18,31 +18,31 @@ def get_jupyter_root_dir() -> str:
     return _jupyter_root_dir
 
 def extract_llm_generated_code(code: str) -> str:
-        if code.endswith("```"):
-            code = code[:-3]
+    if code.endswith("```"):
+        code = code[:-3]
 
-        lines = code.split("\n")
-        if len(lines) < 2:
-            return code
+    lines = code.split("\n")
+    if len(lines) < 2:
+        return code
 
-        num_lines = len(lines)
-        start_line = -1
-        end_line = num_lines
+    num_lines = len(lines)
+    start_line = -1
+    end_line = num_lines
 
-        for i in range(num_lines):
-            if start_line == -1:
-                if lines[i].lstrip().startswith("```"):
-                    start_line = i
-                    continue
-            else:
-                if lines[i].lstrip().startswith("```"):
-                    end_line = i
-                    break
+    for i in range(num_lines):
+        if start_line == -1:
+            if lines[i].lstrip().startswith("```"):
+                start_line = i
+                continue
+        else:
+            if lines[i].lstrip().startswith("```"):
+                end_line = i
+                break
 
-        if start_line != -1:
-            lines = lines[start_line+1:end_line]
+    if start_line != -1:
+        lines = lines[start_line+1:end_line]
 
-        return "\n".join(lines)
+    return "\n".join(lines)
 
 def encrypt_with_password(password: str, data: bytes) -> bytes:
     salt = os.urandom(16)
